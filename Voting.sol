6308185734da838b95a7e5216b7cc798223cd4a1
Rox <roxanne.avinante@gmail.com>
2024-12-04 08:16:13 +0800

    Update Voting.sol
    Defines the logic for voting, including storing candidates, ensuring one vote per user, and emitting events.

--- Voting.sol
+++ Voting.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+    uint public candidatesCount;
+
+    event VotedEvent(uint indexed candidateId);
+
+    constructor() {
+        addCandidate("Democratic: Kamala Harris");
+        addCandidate("Republican: Donald Trump");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "You have already voted.");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate.");
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+
+        emit VotedEvent(_candidateId);
+    }
+}
