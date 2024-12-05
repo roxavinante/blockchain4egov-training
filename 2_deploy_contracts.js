54b5a99721f9703361b5dcec4751fc51991f02f7
Rox <roxanne.avinante@gmail.com>
2024-12-05 10:10:34 +0800

    Update 2_deploy_contracts.js - added descriptions

--- 2_deploy_contracts.js
+++ 2_deploy_contracts.js
@@ -1,5 +1,7 @@
+// Import the compiled contract
 const Voting = artifacts.require("Voting");
 
+// Export a function to deploy the contract
 module.exports = function (deployer) {
-    deployer.deploy(Voting);
+    deployer.deploy(Voting); // Deploy the Voting contract to the blockchain
 };
